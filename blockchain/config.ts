1507fb525530e739889ee47df4eda2c38cb98f11
Diego <diegoguzman366@gmail.com>
2025-12-11 11:23:26 -0400

    project id

--- blockchain/config.ts
+++ blockchain/config.ts
@@ -4,7 +4,7 @@ import { sepolia, mainnet, polygon, arbitrum, optimism, base, scrollSepolia } fr
 // Wagmi configuration with RainbowKit
 export const config = getDefaultConfig({
   appName: "Raffero - Anonymous Raffle",
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || 'be88688ad24de3cfd8fb53eb789cd647',
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
   chains: [scrollSepolia, sepolia, mainnet, polygon, arbitrum, optimism, base],
   ssr: false,
 })
